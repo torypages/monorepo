ae7936d62b82f7266f305af309df3db9aad804de
Tory Law <tory@vantage.com>
2024-07-17 23:49:46 -0400

    -

--- terraform/invoke_tasks/terraform.py
+++ terraform/invoke_tasks/terraform.py
@@ -3,8 +3,9 @@ from invoke import Collection, task
 
 @task
 def apply(c, env: str, docker_tag: str):
+    # TODO should not always auto approve
     c.run(
-        f"terraform apply --var-file=configs/{env}.tfvars --state=state/{env}.tfstate -var='docker_tag={docker_tag}'",
+        f"terraform apply --var-file=configs/{env}.tfvars --state=state/{env}.tfstate -var='docker_tag={docker_tag}' -auto-apply",
         pty=True,
     )
 
