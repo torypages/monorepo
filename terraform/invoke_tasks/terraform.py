492748393e0c019928b1264a89789adeb61341d3
Tory Law <tory@vantage.com>
2024-07-17 23:47:16 -0400

    -

--- terraform/invoke_tasks/terraform.py
+++ terraform/invoke_tasks/terraform.py
@@ -2,9 +2,9 @@ from invoke import Collection, task
 
 
 @task
-def apply(c, env: str):
+def apply(c, env: str, docker_tag: str):
     c.run(
-        f"terraform apply --var-file=configs/{env}.tfvars --state=state/{env}.tfstate",
+        f"terraform apply --var-file=configs/{env}.tfvars --state=state/{env}.tfstate -var='docker_tag={docker_tag}'",
         pty=True,
     )
 
